5e991f8c724df66aa210f662cc69fb6fa3b8f827
cross <crosstown11223@yahoo.com>
2022-03-18 15:13:39 -0400

    CukesRunner.java file updated

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -5,10 +5,9 @@ In the class we will be able to pass pre- & post- conditions to
  */
 
 import com.cydeo.utilities.Driver;
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
+import io.cucumber.java.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 
 
@@ -18,48 +17,54 @@ In the class we will be able to pass pre- & post- conditions to
  */
 
 
+public class Hooks {
 
-    public class Hooks {
-
-        //import from io.cucumber.java not from junit
-        @Before(order = 1)
-        public void setupScenario() {
-            System.out.println("====Setting up browser using cucumber @Before");
-        }
-
-        @Before(value = "@login", order = 2)
-        public void setupScenarioForLogins() {
-            System.out.println("====this will only apply to scenarios with @login tag");
-        }
-
-        @Before(value = "@db", order = 0)
-        public void setupForDatabaseScenarios() {
-            System.out.println("====this will only apply to scenarios with @db tag");
-        }
-
-
-        @After
-        public void teardownScenario() {
-
-            Driver.closeDriver();
-
-
-           // System.out.println("====Closing browser using cucumber @After");
-           // System.out.println("====Scenario ended/ Take screenshot if failed!");
-        }
-
-        @BeforeStep
-        public void setupStep() {
-            System.out.println("--------> applying setup using @BeforeStep");
-        }
-
-        @AfterStep
-        public void afterStep() {
-            System.out.println("--------> applying tearDown using @AfterStep");
-        }
-
-
+    //import from io.cucumber.java not from junit
+    @Before(order = 1)
+    public void setupScenario() {
+        System.out.println("====Setting up browser using cucumber @Before");
     }
+
+    @Before(value = "@login", order = 2)
+    public void setupScenarioForLogins() {
+        System.out.println("====this will only apply to scenarios with @login tag");
+    }
+
+    @Before(value = "@db", order = 0)
+    public void setupForDatabaseScenarios() {
+        System.out.println("====this will only apply to scenarios with @db tag");
+    }
+
+
+    @After  //  this  method will  be executed after my scenario is done
+    public void teardownScenario(Scenario scenario) {  //Scenario scenario comes from java cucumber
+
+        // in Selenium Library there is a method take screenshot
+
+        // scenario.isFailed  ----> this method will return TRUE boolean value
+        if (scenario.isFailed()) {
+            byte[] screenshort = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+
+            scenario.attach(screenshort, "image/png", scenario.getName());
+        }
+        Driver.closeDriver();
+
+        // System.out.println("====Closing browser using cucumber @After");
+        // System.out.println("====Scenario ended/ Take screenshot if failed!");
+    }
+
+    @BeforeStep
+    public void setupStep() {
+        System.out.println("--------> applying setup using @BeforeStep");
+    }
+
+    @AfterStep
+    public void afterStep() {
+        System.out.println("--------> applying tearDown using @AfterStep");
+    }
+
+
+}
 
 
 
