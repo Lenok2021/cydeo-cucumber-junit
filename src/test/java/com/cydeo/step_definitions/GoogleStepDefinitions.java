555f78f8766ce18e1741723975105c8852ea1c7d
cross <crosstown11223@yahoo.com>
2022-03-18 11:35:25 -0400

    CukesRunner.java file updated

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,32 +1,52 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class GoogleStepDefinitions {
 
-    @When("user is on the search page")
-    public void user_is_on_the_search_page() {
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+
+    @When("user types apple and clicks enter")
+    public void user_types_apple_and_clicks_enter() {
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
+    }
+    @Then("user sees apple in the google title")
+    public void user_sees_apple_in_the_google_title() {
+
+        String expectedTitle = "apple - Google Search";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        //Junit assertion accepts first arg as expected, second arg as actual
+        Assert.assertEquals("Title is not as expected!",expectedTitle, actualTitle);
+
+        //Assert.assertTrue(actualTitle.equals(expectedTitle));
+
+    }
+
+
+    @When("user is on Google search page")
+    public void user_is_on_google_search_page() {
 
         Driver.getDriver().get("https://www.google.com");
-
 
     }
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
-
-
         String expectedTitle = "Google";
         String actualTitle = Driver.getDriver().getTitle();
+
         Assert.assertEquals(actualTitle, expectedTitle);
 
-        Driver.closeDriver();
 
 
     }
+
 
 
 
