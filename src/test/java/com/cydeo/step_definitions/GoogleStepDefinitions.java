14bf84f674d81da0d7bf5d485d345978da76efea
cross <crosstown11223@yahoo.com>
2022-03-18 14:33:05 -0400

    CukesRunner.java file updated

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -12,27 +12,31 @@ public class GoogleStepDefinitions {
 
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
+    @When("user types apple and clicks enter")
+    public void user_types_and_clicks_enter2() {
+
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
+
+    }
+
     @When("user types {string} and clicks enter")
     public void user_types_and_clicks_enter(String searchKeyword) {
 
-        googleSearchPage.searchBox.sendKeys(searchKeyword+ Keys.ENTER);
+        googleSearchPage.searchBox.sendKeys(searchKeyword + Keys.ENTER);
 
     }
+
     @Then("user sees {string} in the google title")
     public void user_sees_in_the_google_title(String string) {
 
-        String expectedTitle = string+" - Google Search";
+        String expectedTitle = string + " - Google Search";
         String actualTitle = Driver.getDriver().getTitle();
 
         //Junit assertion accepts first arg as expected, second arg as actual
-        Assert.assertEquals("Title is not as expected!",expectedTitle, actualTitle);
+        Assert.assertEquals("Title is not as expected!", expectedTitle, actualTitle);
 
     }
 
-    @When("user types apple and clicks enter")
-    public void user_types_apple_and_clicks_enter() {
-        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
-    }
     @Then("user sees apple in the google title")
     public void user_sees_apple_in_the_google_title() {
 
@@ -40,7 +44,7 @@ public class GoogleStepDefinitions {
         String actualTitle = Driver.getDriver().getTitle();
 
         //Junit assertion accepts first arg as expected, second arg as actual
-        Assert.assertEquals("Title is not as expected!",expectedTitle, actualTitle);
+        Assert.assertEquals("Title is not as expected!", expectedTitle, actualTitle);
 
         //Assert.assertTrue(actualTitle.equals(expectedTitle));
 
@@ -61,10 +65,5 @@ public class GoogleStepDefinitions {
         Assert.assertEquals(expectedTitle, actualTitle);
 
 
-
     }
-
-
-
-
 }
