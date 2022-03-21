7a9463b724fec9c9b5dd9258f902ce0313970c61
cross <crosstown11223@yahoo.com>
2022-03-21 15:27:28 -0400

    updates BrowserUtils class

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -2,7 +2,11 @@ package com.cydeo.utilities;
 
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class BrowserUtils {
@@ -21,12 +25,12 @@ for given duration
         }
     }
 
-    public static void switchWindowAndVerify( String expectedInUrl, String expectedTitle) {
+    public static void switchWindowAndVerify(String expectedInUrl, String expectedTitle) {
         Set<String> allWindowHandles = Driver.getDriver().getWindowHandles();
 
         for (String each : allWindowHandles) {
 
-      Driver.getDriver().switchTo().window(each);
+            Driver.getDriver().switchTo().window(each);
 
             System.out.println("Current URL " + Driver.getDriver().getCurrentUrl());
 
@@ -39,13 +43,12 @@ for given duration
         Assert.assertTrue(actualTitle.contains(expectedTitle));
 
 
-
-
     }
+
     /*
     This method accepts String "expectedTitle' and accepts if it is true
      */
-    public static void verifyTitle( String expectedTitle){
+    public static void verifyTitle(String expectedTitle) {
 
         Assert.assertEquals(Driver.getDriver().getCurrentUrl(), expectedTitle);
 
@@ -54,11 +57,37 @@ for given duration
 
     /**
      * This method will accept a String as expected value and verify actual URL CONTAINS the value.
+     *
      * @param expectedInURL
      */
-    public static void verifyURLContains(String expectedInURL){
+    public static void verifyURLContains(String expectedInURL) {
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains(expectedInURL));
     }
 
+
+    /**
+     * When you located all options of DropDown (by using getAllOption method)
+     * and you need to get text from each option and return it as a List<String>
+     *
+     * @param dropDownElement
+     * @return
+     */
+
+    public static List<String> convertDropDownOptionsAsString(WebElement dropDownElement) {
+        Select dropdown = new Select(dropDownElement);
+
+        List<WebElement> actualOptionAsWebElement = dropdown.getOptions();
+
+        // we created an empty List<String>
+        //in order to store text from options
+        List<String> actualOptionsAsString = new ArrayList<>();
+
+        for (WebElement each : actualOptionAsWebElement) {
+
+            String text = each.getText(); // get  text  of each option
+            actualOptionsAsString.add(text);
+        }
+        return actualOptionsAsString;
+    }
 
 }
