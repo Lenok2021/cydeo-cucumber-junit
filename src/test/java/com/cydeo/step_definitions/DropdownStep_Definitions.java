d39f48493161b89a48e41c8e3b3e35c5d8839ccf
cross <crosstown11223@yahoo.com>
2022-03-21 14:43:00 -0400

    CukesRunner.java file updated

--- src/test/java/com/cydeo/step_definitions/DropdownStep_Definitions.java
+++ src/test/java/com/cydeo/step_definitions/DropdownStep_Definitions.java
@@ -1,5 +1,6 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
@@ -9,13 +10,14 @@ public class DropdownStep_Definitions {
 
     @Given("User is on the dropdowns page of practice tool")
     public void user_is_on_the_dropdowns_page_of_practice_tool() {
+        Driver.getDriver().get("http://practice.cydeo.com/dropdown");
 
 
     }
     @Then("User should see below info in month dropdown")
-    public void user_should_see_below_info_in_month_dropdown(List<String>) {
+    public void user_should_see_below_info_in_month_dropdown(List<String> expectedMonth) {
 
-
+        System.out.println(expectedMonth);
 
     }
 
