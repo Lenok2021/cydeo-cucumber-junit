1e2d9c4a13c5ebbe4a5a5a1c37625d94fd6f5a3e
cross <crosstown11223@yahoo.com>
2022-03-21 15:55:00 -0400

    updates BrowserUtils class

--- src/test/java/com/cydeo/step_definitions/Web_Table.java
+++ src/test/java/com/cydeo/step_definitions/Web_Table.java
@@ -8,6 +8,9 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.List;
+import java.util.Map;
+
 public class Web_Table {
 
     WebTableLoginPage webTableLoginPage = new WebTableLoginPage();
@@ -17,18 +20,22 @@ public class Web_Table {
         String url = ConfigurationReader.getProperty("web.table.url");
         Driver.getDriver().get(url);
     }
+
     @When("user enters username {string}")
     public void user_enters_username(String string) {
         webTableLoginPage.inputUsername.sendKeys(string);
     }
+
     @When("user enters password {string}")
     public void user_enters_password(String string) {
         webTableLoginPage.inputPassword.sendKeys(string);
     }
+
     @When("user clicks to login button")
     public void user_clicks_to_login_button() {
         webTableLoginPage.loginButton.click();
     }
+
     @Then("user should see url contains orders")
     public void user_should_see_url_contains_orders() {
         BrowserUtils.verifyURLContains("orders");
@@ -40,6 +47,29 @@ public class Web_Table {
         webTableLoginPage.login(username, pw);
 
     }
+
+    @When("User enters below credentials")
+    public void user_enters_below_credentials(Map<String, String> credentials) {
+     /*
+     | username | Test   |
+      | password | Tester |
+     since we have in this format  we used Map
+           */
+
+      //  System.out.println("credentials.get(\"username\") = " + credentials.get("username"));
+      //  System.out.println("credentials.get(\"password\") = " + credentials.get("password"));
+
+        /**
+         * we  used  method  get  from Map
+         * get  method accepts key (username, password) and return the value(credentials)
+         */
+        webTableLoginPage.inputUsername.sendKeys(credentials.get("username"));
+        webTableLoginPage.inputPassword.sendKeys(credentials.get("password"));
+        webTableLoginPage.loginButton.click();
+
+    }
+
+
 }
 
 
--- src/test/resources/features/weTableLogin.feature
+++ src/test/resources/features/weTableLogin.feature
@@ -15,11 +15,9 @@ Feature: User should be able to login using correct credentials
     Then user should see url contains orders
 
 
-
-    @wip
+  @wip
   Scenario: User should be able to see all 12 months in months dropdown
-    Given user is on the login page of web table app
     When User enters below credentials
-      | username     | Test   |
-      | password     | Tester |
+      | username | Test   |
+      | password | Tester |
     Then user should see url contains orders
