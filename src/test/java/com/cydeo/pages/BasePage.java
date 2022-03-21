f195aa295d4c44917c3cb6555927f87ecd2a5509
cross <crosstown11223@yahoo.com>
2022-03-21 17:00:58 -0400

    updates BrowserUtils class

---
+++ src/test/java/com/cydeo/pages/BasePage.java
@@ -0,0 +1,33 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * in this Class we will store WebElements
+ * that are common  for  all Pages of the application
+ */
+
+
+public class BasePage {
+
+    public BasePage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//button [. ='View all orders']")
+    public WebElement viewAllOrders ;
+
+    @FindBy(xpath = "//button [. ='View all products']")
+    public WebElement viewAllProducts ;
+
+    @FindBy(xpath = "//button [. ='Order']")
+    public WebElement order ;
+
+    @FindBy(xpath = "//button [. ='Logout']")
+    public WebElement logoutButton ;
+
+}
