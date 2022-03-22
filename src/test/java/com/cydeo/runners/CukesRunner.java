f44ae2eb6402fbf19fdc415e9f0e8fdc2ca81ad6
cross <crosstown11223@yahoo.com>
2022-03-22 15:04:33 -0400

    updates BrowserUtils class

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 "html:target/cucumber-report.html",
-                "rerun:target/any_name.txt"
+                "rerun:target/rerun.txt"
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
